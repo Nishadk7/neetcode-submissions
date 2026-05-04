002795c142bf2062d634ce3288c7a27081847573
Nishadk7 <59884387+Nishadk7@users.noreply.github.com>
2026-05-03 17:20:02 -0700

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String cur : strs) {
+            int l = cur.length();
+            sb.append(String.format("%03d", l));  // 3‑digit length
+            sb.append(cur);                       // actual string
+        }
+
+        return sb.toString();
+        }
+
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<String>();
+        int i = 0;
+        while(i < str.length()){
+            String num = str.substring(i,i+3);
+            int n = Integer.parseInt(num);
+            String cur = str.substring(i+3,i+3+n);
+            res.add(cur);
+            i = i + 3 + n;
+        }
+        return res;
+    }
+}
